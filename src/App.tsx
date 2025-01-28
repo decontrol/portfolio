c661d578f0145521b9c619bc98a86c059a779cc3
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-01-28 17:30:10 -0500

    cleanup/recommit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className='text-2xl'>Hyungju Kwon</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

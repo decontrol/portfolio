45c09538a123d886143c6dceb09098619a08f657
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-02-05 20:46:48 -0500

    experience page anim fixed

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -18,8 +18,8 @@ const ExperiencePage = () => {
 				<ScrollLinked>
 					{jobs?.map((job: Job, index: number) => (
 						<motion.div
-							initial={{ scale: 0, x: '-100%', opacity: 0 }}
-							animate={{ scale: 1, x: 0, opacity: 1 }}
+							initial={{ scale: 0, opacity: 0 }}
+							animate={{ scale: 1, opacity: 1 }}
 							transition={{
 								duration: 0.5,
 								ease: 'backInOut',

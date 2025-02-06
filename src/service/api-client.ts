e98bf9072407d4c17f15f9ab732016b7e222cf8b
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-02-05 22:10:29 -0500

    updates

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: 'https://portfolio-hoyp.onrender.com/api/v1',
-	// baseURL: BACKEND,
+	baseURL: 'https://hyungjukwon-api.netlify.app/api/v1',
+	// baseURL: 'http://localhost:5000/api/v1',
 });
 
 class APIClient<T> {

329b9f72c283833f656258cdf8e7aaf30d9d8b1e
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-02-14 12:36:07 -0500

    reordered jobs in experience

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -9,6 +9,10 @@ import { motion } from 'motion/react';
 const ExperiencePage = () => {
 	const { data: jobs, isPending, isError, error } = useJobsApi();
 
+	const reorderedJobs = jobs?.sort(
+		(a: Job, b: Job) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+	);
+
 	if (isError) return <h1>{error.message}</h1>;
 	if (isPending) return <Spinner />;
 	return (
@@ -16,7 +20,7 @@ const ExperiencePage = () => {
 			<Heading>Job Experience</Heading>
 			<div className='clear-left'>
 				<ScrollLinked>
-					{jobs?.map((job: Job, index: number) => (
+					{reorderedJobs?.map((job: Job, index: number) => (
 						<motion.div
 							initial={{ scale: 0, x: '100%', opacity: 0 }}
 							animate={{ scale: 1, x: 0, opacity: 1 }}
@@ -27,13 +31,6 @@ const ExperiencePage = () => {
 							}}
 							key={job._id}>
 							<JobItem job={job} />
-							{/* <span>{job._id}</span>
-							<span>{job.company}</span>
-							<span>{job.description}</span>
-							<span>{job.duration}</span>
-							<span>{job.logo}</span>
-							<span>{job.title}</span>
-							<span>{new Date(job.updatedAt).toLocaleString()}</span> */}
 						</motion.div>
 					))}
 				</ScrollLinked>

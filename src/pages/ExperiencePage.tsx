dd02031186f748f3b73a2d91af0463eb2b7d661e
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-11-20 19:40:30 -0500

    mongodb connection fixed

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -22,8 +22,8 @@ const ExperiencePage = () => {
 				<ScrollLinked>
 					{reorderedJobs?.map((job: Job, index: number) => (
 						<motion.div
-							initial={{ scale: 0, x: '100%', opacity: 0 }}
-							animate={{ scale: 1, x: 0, opacity: 1 }}
+							initial={{ x: '100%', opacity: 0 }}
+							animate={{ x: 0, opacity: 1 }}
 							transition={{
 								duration: 0.5,
 								ease: 'backInOut',
